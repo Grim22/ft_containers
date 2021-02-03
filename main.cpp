f0adc0a99f9035bea1ba52901c1203ccd65db8a6
Basile BRUNET <bbrunet@e2r7p20.42.fr>
2021-02-03 10:45:39 +0100

    node insert

--- main.cpp
+++ main.cpp
@@ -3,6 +3,41 @@
 
 int main()
 {
-    map_base<int, char> map(3, 'a');
-    map.insert(4, 'b');
+    node<int, char> *root;
+    root = new node<int, char>;
+    root->left = NULL;
+    root->right = NULL;
+    root->value = 'c';
+    root->key = 8;
+    insert<int, char>(root, 4, 'b');
+    insert<int, char>(root, 12, 'd');
+    insert<int, char>(root, 11, 'd');
+    insert<int, char>(root, 13, 'd');
+    insert<int, char>(root, 2, 'd');
+    insert<int, char>(root, 6, 'd');
+    insert<int, char>(root, 10, 'd');
+    
+    node<int, char> *tmp;
+    tmp = root;
+    while (tmp->left)
+        tmp = tmp->left;
+    std::cout << tmp->key << std::endl; 
+    
+    tmp = root;
+    while (tmp->right)
+        tmp = tmp->right;
+    std::cout << tmp->key << std::endl; 
+    
+    tmp = root;
+    tmp = tmp->left;
+    std::cout << tmp->key << std::endl; 
+    std::cout << tmp->left->key << std::endl; 
+    std::cout << tmp->right->key << std::endl; 
+
+    // map.insert(10, 'd');
+    // map.insert(40, 'f');
+    // map.insert(1, 't');
+    // std::cout << map.base->key << " " << map.base->value;
+    // std::cout << map.base->left->key << " " << map.base->value;
+    // std::cout << map.base->right->key << " " << map.base->value;
 }
--- map.hpp
+++ map.hpp
@@ -1,6 +1,8 @@
 #ifndef MAP_HPP
 # define MAP_HPP
 
+#include <iostream>
+
 template <class Key_type, class Value_type>
 struct node
 {
@@ -8,46 +10,27 @@ struct node
     node* right;
     Key_type key;
     Value_type value;
-    node(): left(NULL), right(NULL), key(Key_type()), value(Value_type())
-    {};
-    node(Key_type key, Value_type val):left(NULL), right(NULL), key(key), value(val)
-    {};
 };
 
 
 template <class key_type, class value_type>
-class map_base
+void insert(node<key_type, value_type> *&root, key_type key, value_type val)
 {
-    typedef node<key_type, value_type> node_type;
-    public:
-    node_type *base;
-
-    public:
-    map_base(): base(nullptr) {};
-    map_base(key_type key, value_type val): base(new node_type(key, val)) {};
-    void insert(key_type key, value_type val)
+    if (root == nullptr)
     {
-        if (this->base == NULL)
-            this->base = new node_type(key, val);
-        else
-            insert(this->base, key, val);
+        root = new node<key_type, value_type>;
+        root->left = 0;
+        root->right = 0;
+        root->key = key;
+        root->value = val;
+        return;
     }
-
-    private:
-    void insert(node_type *node, key_type key, value_type val)
-    {
-        if (node == NULL)
-        {
-            node = new node_type(key, val);
-            return;
-        }
-        if (node->key == key)
-            return ;
-        else if (node->key > key)
-            insert(node->left, key, val);
-        else
-            insert(node->right, key, val);
-    }
-};
+    if (root->key == key)
+        return ;
+    else if (root->key > key)
+        insert(root->left, key, val);
+    else
+        insert(root->right, key, val);
+}
 
 #endif
