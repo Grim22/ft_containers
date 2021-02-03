4dcefdec0609ba67acb8a934ac0961e81d00012d
Basile BRUNET <bbrunet@e2r7p20.42.fr>
2021-02-03 12:47:15 +0100

    search, delete & print_in_order

--- main.cpp
+++ main.cpp
@@ -14,26 +14,46 @@ int main()
     insert<int, char>(root, 10, 'd');
     
     node<int, char> *tmp;
-    tmp = root;
-    while (tmp->left)
-        tmp = tmp->left;
-    std::cout << tmp->key << std::endl; 
+    // tmp = root;
+    // while (tmp->left)
+    //     tmp = tmp->left;
+    // std::cout << tmp->key << std::endl; 
     
-    tmp = root;
-    while (tmp->right)
-        tmp = tmp->right;
-    std::cout << tmp->key << std::endl; 
+    // tmp = root;
+    // while (tmp->right)
+    //     tmp = tmp->right;
+    // std::cout << tmp->key << std::endl; 
     
-    tmp = root;
-    tmp = tmp->left;
-    std::cout << tmp->key << std::endl; 
-    std::cout << tmp->left->key << std::endl; 
-    std::cout << tmp->right->key << std::endl; 
+    // tmp = root;
+    // tmp = tmp->left;
+    // std::cout << tmp->key << std::endl; 
+    // std::cout << tmp->left->key << std::endl; 
+    // std::cout << tmp->right->key << std::endl; 
 
-    // map.insert(10, 'd');
-    // map.insert(40, 'f');
-    // map.insert(1, 't');
-    // std::cout << map.base->key << " " << map.base->value;
-    // std::cout << map.base->left->key << " " << map.base->value;
-    // std::cout << map.base->right->key << " " << map.base->value;
+    // search
+    // tmp = search(root, 2);
+    // if (tmp)
+    //     std::cout << tmp->key << std::endl; 
+    // tmp = search(root, 10);
+    // if (tmp)
+    //     std::cout << tmp->key << std::endl; 
+    // tmp = search(root, 15);
+    // if (tmp)
+    //     std::cout << tmp->key << std::endl; 
+
+    // search min
+    // tmp = search_min(root->right);
+    // std::cout << tmp->key << std::endl;
+
+    print_in_order(root);
+    // delete
+    delete_node(root, 12);
+    delete_node(root, 11);
+    delete_node(root, 10);
+    delete_node(root, 4);
+    delete_node(root, 6);
+    // delete_node(root, 13);
+    // delete_node(root, 11);
+    // std::cout << root->left->key << std::endl;
+
 }
--- map.hpp
+++ map.hpp
@@ -32,9 +32,92 @@ void insert(node<key_type, value_type> *&root, key_type key, value_type val)
         insert(root->right, key, val);
 }
 
-// template <class key_type, class value_type>
-// node<key_type, value_type> search(node<key_type, value_type> root, key_type key, value_type val)
-// {
+template <class key_type, class value_type>
+node<key_type, value_type> *search(node<key_type, value_type> *root, key_type key)
+{
+    if (root == NULL || root->key == key)
+        return root;
+
+    if (root->key > key)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+template <class key_type, class value_type>
+node<key_type, value_type> *search_min(node<key_type, value_type> *root)
+{
+    while (root->left != NULL)
+        root = root->left;
+    return root;
+}
+
+template <class key_type, class value_type>
+node<key_type, value_type> *search_max(node<key_type, value_type> *root)
+{
+    while (root->right != NULL)
+        root = root->right;
+    return root;
+}
+
+template <class key_type, class value_type>
+void delete_node(node<key_type, value_type> *&root, key_type key)
+{
+    // std::cout << "key: " << root->key;
+    // std::cout << "looking for: " << key << std::endl;
+    if (root->key == key)
+    {
+        // delete root
+        // return
+        // case #1: root has no child: delete root and set is to NULL
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            root = NULL;
+            return;
+        }
+        // case #2: root has 1 child: replace root by its child, then delete child
+        if (root->right && root->left == NULL)
+        {
+            root->key = root->right->key;
+            root->value = root->right->value;
+            delete_node(root->right, root->key);
+            return ;
+        }
+        if (root->left && root->right == NULL)
+        {
+            root->key = root->left->key;
+            root->value = root->left->value;
+            delete_node(root->left, root->key);
+            return ;
+        }
+        // case 3: root has 2 childs
+        else
+        {
+            // node<key_type, value_type> *tmp = search_min(root->right);
+            node<key_type, value_type> *tmp = search_max(root->left);
+            root->key = tmp->key;
+            root->value = tmp->value;
+            // delete_node(root->right, tmp->key);
+            delete_node(root->left, tmp->key);
+            return ;
+        }
+    }
+    if (root->key > key)
+        delete_node(root->left, key);
+    else
+        delete_node(root->right, key);
+}
+
+template <class key_type, class value_type>
+void print_in_order(node<key_type, value_type> *&root)
+{
+    if (root->left)
+        print_in_order(root->left);
+    std::cout << root->key << std::endl;
+    if (root->right)
+        print_in_order(root->right);
+}
 
 
 #endif
